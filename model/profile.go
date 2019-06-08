e4a0a5a9cfb159754a9e7133471ef9935fbebda4
pumpkinpie <anemailbox@163.com>
2019-06-09 00:00:02 +0800

    Crawler-V0.1：Single task crawler

---
+++ model/profile.go
@@ -0,0 +1,13 @@
+package model
+
+// 解析对应数据
+type Profile struct {
+	Name     string
+	Gender   string
+	Age      int
+	Height   int
+	Weight   int
+	Income   string
+	Marriage string
+	Address  string
+}
---
+++ zhenai/parser/citylist.go
@@ -0,0 +1,27 @@
+package parser
+
+import (
+	"crawler/engine"
+	"regexp"
+)
+
+const cityListRe = `<a href="(http://www.zhenai.com/zhenghun/[0-9a-z]+)"[^>]*>([^<]+)</a>`
+
+// 解析城市列表
+func ParseCityList(contents []byte) engine.ParseResult {
+	re := regexp.MustCompile(cityListRe)
+	submatch := re.FindAllSubmatch(contents, -1)
+	result := engine.ParseResult{}
+	limit := 1 // 限制搜索次数
+	for _, item := range submatch {
+		result.Items = append(result.Items, string(item[2]))
+		result.Requests = append(result.Requests, engine.Request{
+			Url:       string(item[1]),
+			ParseFunc: ParseCity,
+		})
+		if limit--; limit == 0 {
+			break
+		} // 控制次数
+	}
+	return result
+}

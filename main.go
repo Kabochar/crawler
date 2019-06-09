63f9b39244dcd706b30f821a43e5ce8063cf511b
pumpkinpie <anemailbox@163.com>
2019-06-09 12:08:20 +0800

    Crawler-V0.3.1：add more city

--- main.go
+++ main.go
@@ -12,8 +12,14 @@ func main() {
 		Scheduler:   &scheduler.QueuedScheduler{}, // 这里调用并发调度器
 		WorkerCount: 100,
 	}
+	//e.Run(engine.Request{
+	//	Url:       "http://www.zhenai.com/zhenghun/",
+	//	ParseFunc: parser.ParseCityList,
+	//})
+
+	// 捕捉底部更多城市链接
 	e.Run(engine.Request{
-		Url:       "http://www.zhenai.com/zhenghun",
+		Url:       "http://www.zhenai.com/zhenghun/shanghai",
 		ParseFunc: parser.ParseCityList,
 	})
 }
--- zhenai/parser/city.go
+++ zhenai/parser/city.go
@@ -5,14 +5,17 @@ import (
 	"regexp"
 )
 
-var cityRe = regexp.MustCompile(`<a href="(http://album.zhenai.com/u/[0-9]+)"[^>]*>([^<]+)</a>`)
+var profileRe = regexp.MustCompile(`<a href="(http://album.zhenai.com/u/[0-9]+)"[^>]*>([^<]+)</a>`)
 
 // 用户性别正则，因为在用户详情页没有性别信息，所以在用户性别在用户列表页面获取
 var sexRe = regexp.MustCompile(`<td width="180"><span class="grayL">性别：</span>([^<]+)</td>`)
 
+// 底部城市页
+var cityUrlsRe = regexp.MustCompile(`href="http://www.zhenai.com/zhenghun/[^"]+]`)
+
 // 城市页面用户解析器
 func ParseCity(bytes []byte) engine.ParseResult {
-	cityMatch := cityRe.FindAllSubmatch(bytes, -1)
+	cityMatch := profileRe.FindAllSubmatch(bytes, -1)
 	gendermatch := sexRe.FindAllSubmatch(bytes, -1)
 
 	result := engine.ParseResult{}
@@ -29,5 +32,16 @@ func ParseCity(bytes []byte) engine.ParseResult {
 			},
 		})
 	}
+
+	// 匹配城市 底部更多
+	matches := cityUrlsRe.FindAllSubmatch(bytes, -1)
+	for _, m := range matches {
+		result.Requests = append(result.Requests,
+			engine.Request{
+				Url:       string(m[1]),
+				ParseFunc: ParseCity,
+			})
+	}
+
 	return result
 }
